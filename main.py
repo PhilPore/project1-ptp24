483e652688b86cdc57fec3efa8b52a1f9ff86c6a
EC2 Default User <ec2-user@ip-172-31-39-30.ec2.internal>
2021-02-03 00:54:14 +0000

    more fun. Heh.

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import os 
 import flask
+
 from flask import Flask, render_template
+from dotenv import load_dotenv, find_dotenv
+import requests
+
+
+load_dotenv(find_dotenv())
+
 
 app = Flask(__name__)
-
 @app.route('/')
 def init_main():
+    
+    
     print("Kernel")
     return render_template("index.html") #will have to pass data in here.
 
