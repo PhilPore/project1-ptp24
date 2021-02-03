e0591058cf19b121188b05e84fe8da7119e170ec
EC2 Default User <ec2-user@ip-172-31-39-30.ec2.internal>
2021-02-03 01:45:23 +0000

    Added prelims for getting info from the spotify api, and did some testing with html

--- main.py
+++ main.py
@@ -6,16 +6,48 @@ from dotenv import load_dotenv, find_dotenv
 import requests
 
 
+#load_dotenv(find_dotenv())
 load_dotenv(find_dotenv())
+auth_url = 'https://accounts.spotify.com/api/token'
+clientid = os.getenv("CLI_ID")
+client_secret = os.getenv("CLI_SEC")
+Auth_Req = requests.post(auth_url, {
+    'grant_type': 'client_credentials',
+    'client_id': clientid,
+    'client_secret': client_secret,
+})
+auth_tok = Auth_Req.json()['access_token']
+#print(auth_tok)
+
+header = {
+   'Authorization': "Bearer {}".format(auth_tok)
+}
+
+url="https://api.spotify.com/v1/browse/new-releases"
+
+
 
 
 app = Flask(__name__)
 @app.route('/')
 def init_main():
+    print("Killroy was here")
+    header = {
+       'Authorization': "Bearer {}".format(auth_tok)
+    }
     
+    url="https://api.spotify.com/v1/browse/new-releases"
     
-    print("Kernel")
-    return render_template("index.html") #will have to pass data in here.
+    parameters = {'limit': 10}
+    
+    response = requests.get(url, headers = header, params = parameters)
+    
+    data = response.json()
+    resources = []
+    for i in range(10):
+        resources.append(data['albums']['items'][i]['name'])
+    
+    return render_template("index.html",resources=resources) #will have to pass data in here.
 
 app.run(
     port=int(os.getenv('PORT', 8080)),
